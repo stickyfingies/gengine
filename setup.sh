0b158d0c80082b8e60fbb327ba6da51a820e4dae
sticky <sethtraman@gmail.com>
2024-09-20 20:46:06 -0500

    Update setup.sh to be bash-friendly

--- setup.sh
+++ setup.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/bash
+cd dependencies
+
+####
 
 echo "==Setup Emscripten=="
+./emsdk/emsdk install latest
+./emsdk/emsdk activate latest
+source ./emsdk/emsdk_env.sh
 
-alias emsdk=./dependencies/emsdk/emsdk
-emsdk install latest
-emsdk activate latest
-source ./dependencies/emsdk/emsdk_env.sh
-echo $(whereis emcc)
+####
 
 echo "==Setup vcpkg=="
-
-chmod +x ./dependencies/vcpkg/bootstrap-vcpkg.sh
-./dependencies/vcpkg/bootstrap-vcpkg.sh
-
-alias vcpkg=./dependencies/vcpkg/vcpkg
-
-vcpkg install glfw3
-vcpkg install bullet3
-vcpkg install assimp:x64-linux
-vcpkg install assimp:wasm32-emscripten
-vcpkg install glad
-vcpkg install glm:x64-linux
-vcpkg install glm:wasm32-emscripten
-vcpkg install imgui[core,glfw-binding,vulkan-binding]
-vcpkg install vulkan
+chmod +x ./vcpkg/bootstrap-vcpkg.sh
+./vcpkg/bootstrap-vcpkg.sh
+./vcpkg/vcpkg install glfw3
+./vcpkg/vcpkg install bullet3
+./vcpkg/vcpkg install assimp:x64-linux
+./vcpkg/vcpkg install assimp:wasm32-emscripten
+./vcpkg/vcpkg install glad
+./vcpkg/vcpkg install glm:x64-linux
+./vcpkg/vcpkg install glm:wasm32-emscripten
+./vcpkg/vcpkg install imgui[core,glfw-binding,vulkan-binding]
+./vcpkg/vcpkg install vulkan
