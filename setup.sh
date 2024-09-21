07349972c3d827bf02bc721e2b700aa35a3f7770
sticky <sethtraman@gmail.com>
2024-09-20 20:22:11 -0500

    Use submodules for dependencies

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/sh
+
+echo "==Setup Emscripten=="
+
+alias emsdk=./dependencies/emsdk/emsdk
+emsdk install latest
+emsdk activate latest
+source ./dependencies/emsdk/emsdk_env.sh
+
+echo "==Setup vcpkg=="
+
+chmod +x ./dependencies/vcpkg/bootstrap-vcpkg.sh
+./dependencies/vcpkg/bootstrap-vcpkg.sh
+
+alias vcpkg=./dependencies/vcpkg/vcpkg
+
+vcpkg install glfw3
+vcpkg install bullet3
+vcpkg install assimp:x64-linux
+vcpkg install assimp:wasm32-emscripten
+vcpkg install glad
+vcpkg install glm:x64-linux
+vcpkg install glm:wasm32-emscripten
+vcpkg install imgui[core,glfw-binding,vulkan-binding]
+vcpkg install vulkan
