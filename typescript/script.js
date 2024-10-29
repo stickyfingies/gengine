bd890dd334cccaaf40af65a72fd92178c6a068bd
sticky <sethtraman@gmail.com>
2024-10-28 22:35:32 -0500

    Add dukglue bindings for a basic GLFWwindow* class

--- typescript/script.js
+++ typescript/script.js
@@ -1,1 +1,15 @@
-print("Hello, " + adder(1, 2) + " JavaScript!");
+// Create a new window
+const window = new Window();
+
+// Core game loop
+while (!window.shouldClose()) {
+
+    window.update();
+
+    if (window.getEsc()) {
+        window.setShouldClose(true);
+    }
+}
+
+// Adios!
+print("Goodbye, JS!");
