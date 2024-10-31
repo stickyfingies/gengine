20bb856cb673d64e311b6aa3adc60ee51888d4ce
sticky <sethtraman@gmail.com>
2024-10-30 20:05:41 -0500

    Document some stuff

--- examples/javascript.js
+++ examples/javascript.js
@@ -1,10 +1,31 @@
+/**
+ * This file uses JS bindings from javascript.cpp to create a game scene.
+ */
+
 print("Hello, JS!");
 
+/**
+ * This function is called after javascript.cpp starts the core engine systems.
+ * @param {*} scene a wrapper around the C++ SceneBuilder API
+ * @todo change laodModel() to accept arrays of data
+ */
 function create(scene) {
-    print("Creating scene...");
 
-    loadModel(scene, "./data/spinny.obj", new VisualModelSettings(false, true, false));
-    loadModel(scene, "./data/map.obj", new VisualModelSettings(true, true, false));
+    const models = [
+        { path: "./data/spinny.obj", flip_uvs: false, flip_tris: true },
+        { path: "./data/map.obj", flip_uvs: true, flip_tris: true }
+    ];
+
+    for (var i = 0; i < models.length; i++) {
+        loadModel(scene, models[i].path, new VisualModelSettings(models[i].flip_uvs, models[i].flip_tris, false));
+    }
+
+    // const player = { location: [20, 100, 20], shape: CAPSULE, mass: 70, vanity: "./data/spinny.obj" };
+
+    // const player = new Entity()
+    //     .has(new Location(20, 100, 20))
+    //     .has(new SolidCapsule({ mass: 70 }))
+    //     .has(new Model3D("./data/spinny.obj"));
 
     // player
     const playerTransform = new Transform().translate(20, 100, 20);
