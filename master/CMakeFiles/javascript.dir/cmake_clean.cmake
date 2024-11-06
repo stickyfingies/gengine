1d90e45884e144a57a848fa41f4a0cf598257e53
stickyfingies <stickyfingies@users.noreply.github.com>
2024-11-06 03:56:13 +0000

    deploy: 955600fb09dae1357e8450816eb2fe4e24b5c3b1

---
+++ master/CMakeFiles/javascript.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/javascript.dir/__/core/ffi/js.web.cpp.o"
+  "CMakeFiles/javascript.dir/__/core/ffi/js.web.cpp.o.d"
+  "CMakeFiles/javascript.dir/__/core/main.cpp.o"
+  "CMakeFiles/javascript.dir/__/core/main.cpp.o.d"
+  "javascript/index.js"
+  "javascript/index.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/javascript.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ master/CMakeFiles/native.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/gengine/gengine/core/main.cpp" "examples/CMakeFiles/native.dir/__/core/main.cpp.o" "gcc" "examples/CMakeFiles/native.dir/__/core/main.cpp.o.d"
+  "/home/runner/work/gengine/gengine/examples/native.cpp" "examples/CMakeFiles/native.dir/native.cpp.o" "gcc" "examples/CMakeFiles/native.dir/native.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
