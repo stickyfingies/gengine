6751821a9676fa83440dae38737c8c91e79d44da
stickyfingies <stickyfingies@users.noreply.github.com>
2024-11-06 06:59:24 +0000

    deploy: 8266f0dec6ee16d394e7a837be55d55471076eb3

---
+++ hotfix-vitepress/CMakeFiles/native.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/native.dir/__/core/main.cpp.o"
+  "CMakeFiles/native.dir/__/core/main.cpp.o.d"
+  "CMakeFiles/native.dir/native.cpp.o"
+  "CMakeFiles/native.dir/native.cpp.o.d"
+  "native/index.js"
+  "native/index.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/native.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
