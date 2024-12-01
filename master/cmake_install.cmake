6e607141686da5be04b298d84d8f8117f7881b44
stickyfingies <stickyfingies@users.noreply.github.com>
2024-12-01 03:46:29 +0000

    deploy: ba33f263b4c490dbeef32b31097705eb1784430e

--- master/cmake_install.cmake
+++ master/cmake_install.cmake
@@ -55,3 +55,9 @@ if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT
     )
 endif()
 
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+if(CMAKE_INSTALL_LOCAL_ONLY)
+  file(WRITE "/home/runner/work/gengine/gengine/artifacts/web-gl-app/examples/install_local_manifest.txt"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
+endif()
